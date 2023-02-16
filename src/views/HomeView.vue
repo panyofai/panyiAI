ffe9ef4b6157a002d925d13bc6130785fcba9b52
小滴课堂大钊 <>
2023-02-16 22:58:25 +0800

    配置unocss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div text="red 5">
+    首页页面
+</div>
 </template>
